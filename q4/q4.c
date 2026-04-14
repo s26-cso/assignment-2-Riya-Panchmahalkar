bc9ce28dcc7b12b731a8fbcf02c697d7cecfc113
Panchmahalkar Riya <riyapanchmahalkar@gmail.com>
2026-04-14 16:34:03 +0530

    Q4 solved

---
+++ q4/q4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dlfcn.h>
+
+int main() {
+    char op[16];
+    int a,b;
+    void *handle = NULL;
+    int (*operation_func)(int,int);
+    while (scanf("%s %d %d",op,&a,&b) != EOF) {
+      
+        char lib_name[32];
+        snprintf(lib_name,sizeof(lib_name),"./lib%s.so",op);
+
+        if (handle != NULL) {
+            dlclose(handle);
+            handle = NULL;
+        }
+        handle = dlopen(lib_name,RTLD_NOW);
+        if (!handle) continue;
+
+        operation_func = (int (*)(int, int))dlsym(handle,op);
+
+        if (operation_func) printf("%d\n", operation_func(a,b));
+    }
+    if (handle) dlclose(handle);
+    return 0;
+}
